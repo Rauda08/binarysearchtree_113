1e37ac9b652643cd65af9498f3dc1630cd0af44b
rauda08 <raudajnnh08@gmail.com>
2023-06-29 22:40:54 +0700

    membuat left child dan parent point ke node baru

--- binarysearchtree_113/binarysearchtree_113.cpp
+++ binarysearchtree_113/binarysearchtree_113.cpp
@@ -41,9 +41,14 @@ public:
 
 		if (parent == NULL) // if te parent is NULL (Tree is empty)
 		{
-
+			ROOT = newNode; // Mark the new node as ROOT 
+			return; // Exit
 		}
 
+		if (element < parent->info) // If the value in the date field of the new node is less than that of the parent
+		{
+			parent->leftchild = newNode; // Make the left child of the parent point to the new node
+		}
 
 	}
 };
