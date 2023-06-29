ecc34b0b1a044a23b64a1b13edec8659c53ef702
rauda08 <raudajnnh08@gmail.com>
2023-06-29 22:11:27 +0700

    menginput karakter dan membuat nama elemen

--- binarysearchtree_113/binarysearchtree_113.cpp
+++ binarysearchtree_113/binarysearchtree_113.cpp
@@ -21,4 +21,14 @@ public:
 class BinaryTree
 {
 public:
+	Node* ROOT;
+
+	BinaryTree()
+	{
+		ROOT = NULL; // Initializing ROOT to null
+	}
+
+	void insert(string element) // Insert a node in the binary search tree
+	{
+	}
 };
