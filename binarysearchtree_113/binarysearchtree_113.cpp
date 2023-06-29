8851fbbe36301234a7c36dcb26b2459e170c5ed4
rauda08 <raudajnnh08@gmail.com>
2023-06-29 22:24:54 +0700

    menentukan blok kode yang akan dieksekusi menggunakan "if"

--- binarysearchtree_113/binarysearchtree_113.cpp
+++ binarysearchtree_113/binarysearchtree_113.cpp
@@ -30,5 +30,20 @@ public:
 
 	void insert(string element) // Insert a node in the binary search tree
 	{
+		Node* newNode = new Node(element, NULL, NULL); // Allocate memory for the new node
+		newNode->info = element; // Assign value to the data field of the new node
+		newNode->leftchild = NULL;// Make the left child of thhe new node point to NULL
+		newNode->rightchild = NULL; // Make the right child of the new node point to NULL
+
+		Node* parent = NULL;
+		Node* currentNode = NULL;
+		search(element, parent, currentNode); // Locate the node which will be the parent of the node to be inserted
+
+		if (parent == NULL) // if te parent is NULL (Tree is empty)
+		{
+
+		}
+
+
 	}
 };
